cbd840181cf55db00165e79e7a36be9fb0313cc5
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-24 13:39:40 -0500

    bcrypt hash

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+const memoryDB = []
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
@@ -18,9 +18,23 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_SYNC
 
-async() => {
-    const hashing = await bcrypt.hash(myPlaintextPassword, saltRounds)
-}
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash)
+
+    bcrypt.hash('passw0rd!', 13, (err, hash) => {
+        console.log(hash);
+        //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    });
+    bcrypt.compare(myPlaintextPassword, hash,(err, res) => {
+        console.log(res)
+        bcrypt.compare('passw0rd!', hash, (err, res) => {
+          console.log(res); //true
+        });
+    })
+
+      
+})
+
 
 //END_SYNC
 
